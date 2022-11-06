82d001ed3a034a2a8d2db99b63860b27ab5b6622
SinaSaeedi13 <116140060+SinaSaeedi13@users.noreply.github.com>
2022-11-06 19:03:46 +0330

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,265 @@
+def seconds_difference(time_1, time_2):
+    """ (number, number) -> number
+
+    Return the number of seconds later that a time in seconds
+    time_2 is than a time in seconds time_1.
+        
+    >>> seconds_difference(1800.0, 3600.0)
+    1800.0
+    >>> seconds_difference(3600.0, 1800.0)
+    -1800.0
+    >>> seconds_difference(1800.0, 2160.0)
+    360.0
+    >>> seconds_difference(1800.0, 1800.0)
+    0.0
+    """
+    time_3 = time_2 - time_1
+    return time_3
+
+
+
+
+
+
+
+
+
+
+def hours_difference(time_1, time_2):
+    """ (number, number) -> float
+
+    Return the number of hours later that a time in seconds
+    time_2 is than a time in seconds time_1.
+        
+    >>> hours_difference(1800.0, 3600.0)
+    0.5
+    >>> hours_difference(3600.0, 1800.0)
+    -0.5
+    >>> hours_difference(1800.0, 2160.0)
+    0.1
+    >>> hours_difference(1800.0, 1800.0)
+    0.0
+    """
+    time_3_hour=(time_2 - time_1) / 3600
+    return time_3_hour
+
+
+
+
+
+
+
+
+
+
+
+def to_float_hours(hours, minutes, seconds):
+    """ (int, int, int) -> float
+
+    Return the total number of hours in the specified number
+    of hours, minutes, and seconds.
+
+    Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
+
+    >>> to_float_hours(0, 15, 0)
+    0.25
+    >>> to_float_hours(2, 45, 9)
+    2.7525
+    >>> to_float_hours(1, 0, 36)
+    1.01
+    """
+
+    to_num=hours + (minutes/60) + (seconds/3600)
+    return to_num
+
+
+
+
+
+
+
+
+
+
+
+
+def to_24_hour_clock(hours):
+    """ (number) -> number
+
+    hours is a number of hours since midnight. Return the
+    hour as seen on a 24-hour clock.
+
+    Precondition: hours >= 0
+
+    >>> to_24_hour_clock(24)
+    0
+    >>> to_24_hour_clock(48)
+    0
+    >>> to_24_hour_clock(25)
+    1
+    >>> to_24_hour_clock(4)
+    4
+    >>> to_24_hour_clock(28.5)
+    4.5
+    """
+    while hours >= 24 :
+        hours = hours - 24
+    num=hours
+    return num
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Write your get_hours function definition here:
+def get_hours(num):
+    '''
+    num -> (int)
+    this function will take any number(not negative) as seconds
+    and turn it into hours but the diffrence is that
+    it won't take the decimal part of that number
+
+   get_hours(3800) : 1
+   '''
+    hours=num//3600
+    return hours
+### Write your get_minutes function definition here:
+def get_minutes(num):
+     '''
+    num -> (int)
+    this function will take any number(not negative) as seconds
+    and extract the hours of it and return the rest as minutes but
+    the diffrence is that it wont't take the decimal part of that number
+
+   get_minutes(3800) : 3
+   '''
+
+     minutes=num%3600
+     answer=minutes//60
+     return answer
+
+### Write your get_seconds function definition here:
+def get_seconds(num):    
+   '''
+    num -> (int)
+    this function will take any number(not negative) as seconds
+    and extract the hours and minutes of it and return everything that is left
+    as seconds
+
+   get_minutes(3800) : 20
+   '''
+   minutes=num%3600
+   seconds=minutes%60
+   return seconds
+
+
+
+
+
+
+
+
+
+
+
+def time_to_utc(utc_offset, time):
+    """ (number, float) -> float
+
+    Return time at UTC+0, where utc_offset is the number of hours away from
+    UTC+0.
+
+    >>> time_to_utc(+0, 12.0)
+    12.0
+    >>> time_to_utc(+1, 12.0)
+    11.0
+    >>> time_to_utc(-1, 12.0)
+    13.0
+    >>> time_to_utc(-11, 18.0)
+    5.0
+    >>> time_to_utc(-1, 0.0)
+    1.0
+    >>> time_to_utc(-1, 23.0)
+    0.0
+    """
+    hours=time - utc_offset
+    while hours >= 24 :
+        hours = hours - 24
+    num=hours
+    return num
+
+
+
+
+
+
+
+
+def time_from_utc(utc_offset, time):
+    """ (number, float) -> float
+
+    Return UTC time in time zone utc_offset.
+
+    >>> time_from_utc(+0, 12.0)
+    12.0
+    >>> time_from_utc(+1, 12.0)
+    13.0
+    >>> time_from_utc(-1, 12.0)
+    11.0
+    >>> time_from_utc(+6, 6.0)
+    12.0
+    >>> time_from_utc(-7, 6.0)
+    23.0
+    >>> time_from_utc(-1, 0.0)
+    23.0
+    >>> time_from_utc(-1, 23.0)
+    22.0
+    >>> time_from_utc(+1, 23.0)
+    0.0
+    """
+    hours=time + utc_offset
+    while hours >= 24 :
+        hours = hours - 24
+    num=hours
+    return num
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    

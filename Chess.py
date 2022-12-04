a9b6f3ed8d52ef4709ca73164e59ee04fc09509b
SinaSaeedi13 <116140060+SinaSaeedi13@users.noreply.github.com>
2022-12-04 22:46:29 +0330

    Add files via upload

---
+++ Chess.py
@@ -0,0 +1,30 @@
+#incomplete , will be edited soon
+
+Letter_up=['A','B','C','D','E','F','G','H']
+Letter_down=['a','b','c','d','e','f','g','h']
+HB=input(' enter horizontal position of the bishop (A,B,C,D,E,F,G,H): ')
+if HB not in Letter_up and HB not in Letter_down :
+    print ('horizontal input for bishop is not from the list')
+else:
+    VB=input(' enter vertical position of the bishop (1,2,3,4,5,6,7,8): ')
+    if VB not in Letter_up and VB not in Letter_down :
+        print ('vertical input for bishop is not from the list')
+        else:
+            HK=input(' enter horizontal position of the knight (A,B,C,D,E,F,G,H): ')
+            if VB not in Letter_up and VB not in Letter_down :
+                print ('horizontal input for knight is not from the list')
+                else:
+                    VK=input('Please enter vertical position of the knight (1,2,3,4,5,6,7,8): ')
+                    if VK not in Letter_up and VK not in Letter_down :
+                        print ('vertical input for knight is not from the list')
+                        
+                        
+                        
+                    
+                
+                
+            
+            
+        
+        
+
